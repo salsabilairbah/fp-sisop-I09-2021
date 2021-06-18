a798eed8351885836560ac83bebc7af92cec13a8
Salsabila <bella.irbah@gmail.com>
2021-06-18 17:48:52 +0700

    client

--- client/Program_dump_client.c
+++ client/Program_dump_client.c
@@ -13,13 +13,13 @@
 void createTable(char data[]){
 	
 }
-// Driver code
+
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
 	struct sockaddr_in servaddr, cliaddr;
 	
-	// Creating socket file descriptor
+
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
@@ -28,12 +28,12 @@ int main() {
 	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
 	
-	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
+
+	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
 	
-	// Bind the socket with the server address
+
 	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
 			sizeof(servaddr)) < 0 )
 	{
@@ -43,21 +43,21 @@ int main() {
 	
 	int len, n;
 
-	len = sizeof(cliaddr); //len is value/resuslt
-    //ini diterima(receive) dari client ke buffer
+	len = sizeof(cliaddr); 
+
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
 				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
 				&len);
 	buffer[n] = '\0';
     char msg1[MAXLINE],msg2[MAXLINE],temp[MAXLINE];
-	// buffer dimasukkin ke temporary
+
     strncpy(temp,buffer,sizeof(buffer));
-    //buffer diambil kata pertama sama kedua nya
+
     char *msg = strtok(buffer," ");
 	strncpy(msg1,buffer,sizeof(buffer));
     msg = strtok(NULL," ");
     strncpy(msg2,msg,sizeof(buffer));
-    //ini dicek kata pertama sama keduanya, kalo create table blablabla
+
     msg = strtok(NULL," ");
     if(strcmp(msg1,"CREATE")==0){
         if(strcmp(msg2,"TABLE" )==0){
--- client/program_client.c
+++ client/program_client.c
@@ -1,4 +1,3 @@
-// Client side implementation of UDP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,14 +16,14 @@ int main() {
     char buffer[MAXLINE];
     char data[50];
     char *clear[50];
-    //minta input ke data
+    
     
     while((data[i++] = getchar()) != '\n');
     struct sockaddr_in servaddr;
     clear = strtok(data,"\n");
     printf(clear);
   
-    // Creating socket file descriptor
+    
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
@@ -32,13 +31,13 @@ int main() {
   
     memset(&servaddr, 0, sizeof(servaddr));
       
-    // Filling server information
+    
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
       
     int n, len;
-     //datanya di send ke dump_client lewat sendto(line 39) 
+    
     sendto(sockfd, (const char *)clear, strlen(clear),
         MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
             sizeof(servaddr));
